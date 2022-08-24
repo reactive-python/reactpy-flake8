155c15af24fed21e58d48123ac3d3d44404058bb
rmorshea <ryan.morshead@gmail.com>
2022-08-23 22:45:12 -0700

    improve component decorator check

--- flake8_idom_hooks/utils.py
+++ flake8_idom_hooks/utils.py
@@ -1,6 +1,9 @@
 import ast
+import re
 from contextlib import contextmanager
 from typing import Any, Iterator, List, Tuple
+
+_COMPONENT_DECORATOR_NAME_PATTERN = re.compile(r"^(idom.(\w+\.)*)?component$")
 
 
 @contextmanager
@@ -28,17 +31,25 @@ def is_hook_def(node: ast.FunctionDef) -> bool:
 
 
 def is_component_def(node: ast.FunctionDef) -> bool:
-    return any(
-        is_idom_component_decorator(decorator) for decorator in node.decorator_list
-    )
-
-
-def is_idom_component_decorator(node: Any) -> bool:
-    return getattr(node, "id", None) == "component" or (
-        getattr(getattr(node, "value", None), "id", None) == "idom"
-        and getattr(node, "attr", None) == "component"
-    )
+    return any(map(_is_component_decorator, node.decorator_list))
 
 
 def is_hook_function_name(name: str) -> bool:
     return name.lstrip("_").startswith("use_")
+
+
+def _is_component_decorator(node: ast.AST) -> bool:
+    return (
+        _COMPONENT_DECORATOR_NAME_PATTERN.match(
+            ".".join(reversed(list(_get_dotted_name(node))))
+        )
+        is not None
+    )
+
+
+def _get_dotted_name(node: ast.AST) -> Iterator[str]:
+    while isinstance(node, ast.Attribute):
+        yield node.attr
+        node = node.value
+    if isinstance(node, ast.Name):
+        yield node.id
